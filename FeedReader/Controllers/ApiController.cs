d80bec2f9c36e233fd28479f070df637b67fbd42
bambidarra <bambi@bambidarra.com>
2011-03-18 08:54:23 +0000

    Api stuff.

--- FeedReader/Controllers/ApiController.cs
+++ FeedReader/Controllers/ApiController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FeedReader.Classes;
+using FeedReader.ViewModels;
 
 namespace FeedReader.Controllers
 {
@@ -11,7 +13,22 @@ namespace FeedReader.Controllers
         [HttpPost]
         public ActionResult GetFeeds(string username, string password, int? page)
         {
-            return View();
+            if (!UserInfo.LogIn(username, password))
+            {
+                Response.Write("Login Failed");
+                return null;
+            }
+
+            if (page <= 0) page = null;
+            DataBase DB = DataBase.GetInstance();
+            DB.ClearInternalCache();
+
+            var q = (from feed in UserInfo.Info.UserFeeds
+                     from feedItem in feed.Feed.FeedItems
+                     where !UserInfo.Info.UserFeedItems.Any(f => f.FeedItemID == feedItem.ID && f.Read)
+                     select feedItem);
+
+            return View(new ViewFeedsViewModel { CurrentPage = page ?? 1, FeedItems = q.OrderByDescending(f => f.Created).Skip(page.HasValue ? Math.Abs((page.Value - 1) * Properties.Settings.Default.PageLength) : 0).Take(Properties.Settings.Default.PageLength), Pages = (int)Math.Ceiling(q.Count() / (double)Properties.Settings.Default.PageLength) });
         }
     }
 }
